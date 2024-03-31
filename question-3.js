fb48529ff5354fb6604d31f81618cee69ff9b111
keithowino <designsolutions1629@gmail.com>
2024-04-01 01:07:54 +0300

    Add comments to the third solution.

--- question-3.js
+++ question-3.js
@@ -3,15 +3,17 @@ const prompt = require("prompt-sync")({sigint : true});
 
 let div = "\n============\n\n";
 
-let inputArr = prompt("Input an array of numbers: ");
+let inputArr = prompt("Input an array of numbers: ");    // Here we ask the user to input an array of numbers.
 
+// After input submission we evaluate to determine whether it is valid to be processed as an array.
 function validateInput(){
   let output;
 
   if(inputArr[0] === "[" && inputArr[inputArr.length - 1] === "]"){
+    // Our input is valid, now we have the array we need.
     output = validPrime(inputArr.slice(1, -1).split(',').map(Number));
   }else{
-    output = "Your input was not an array";
+    output = "Your input was not an array";    // If input was not an array we throw an error message.
   };
 
   return output;
@@ -19,6 +21,9 @@ function validateInput(){
 
 function validPrime(arr){
 
+  /*
+  The validated array is passed through an array filter iteration method. Goal is to filter out prime numbers.
+  */
   let newArr = arr.filter(allPrime);
 
   function allPrime(value){
